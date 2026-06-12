1613877e6a1741792b5b4e118a80078d1bba21f7
Fiore Palermo <fiorepalermo@gmail.com>
2026-06-12 14:33:25 +0200

    add main js

---
+++ esercizio-3/README.md
@@ -0,0 +1,6 @@
+# Esercizio
+
+Stai creando un contatore di click; incrementa il numero visualizzato nel pulsante dopo ogni click.
+
+## Ragionamento
+
--- esercizio-3/assets/main.js
+++ esercizio-3/assets/main.js
@@ -1,2 +1,12 @@
+console.log("js caricato");
+
+const bottoneContatore = document.getElementById('conta');
+
+let conteggio = 0;
 
 
+bottoneContatore.addEventListener('click', function () {
+    conteggio++;
+
+    bottoneContatore.textContent = `Click: ${conteggio}`;
+});
